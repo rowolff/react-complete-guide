c4285363951d6cea37690b59f80a11f815b4224c
Robert Wolff <ro.wolff@gmx.net>
2021-08-18 16:16:56 +0200

    Add ExpenseItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>This is also visible</p>
+      <ExpenseItem />
     </div>
   );
 }
