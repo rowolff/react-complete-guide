2b0ab5bac8f7e6d66b74aa0f80e12d070e7e3668
Robert Wolff <ro.wolff@gmx.net>
2021-08-19 13:19:18 +0200

    Introduce local state

--- src/components/Expenses/ExpenseItem.tsx
+++ src/components/Expenses/ExpenseItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './ExpenseItem.css';
 import Card from '../UI/Card';
@@ -10,7 +10,17 @@ interface Props {
   amount: number;
 }
 
-const ExpenseItem: React.FC<Props> = ({ date, title, amount }) => {
+const ExpenseItem: React.FC<Props> = ({
+  date,
+  title: titleFromProps,
+  amount,
+}) => {
+  const [title, setTitle] = useState<string>(titleFromProps);
+
+  const handleTitleChange = () => {
+    setTitle('Title updated');
+  };
+
   return (
     <Card classNames={['expense-item']}>
       <ExpenseDate date={date} />
@@ -18,6 +28,7 @@ const ExpenseItem: React.FC<Props> = ({ date, title, amount }) => {
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={handleTitleChange}>Change Title</button>
     </Card>
   );
 };
