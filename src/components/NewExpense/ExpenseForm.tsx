3a9fc0ff1ea72094b90c98ed418e485170f4d38f
Robert Wolff <ro.wolff@gmx.net>
2021-08-19 13:51:59 +0200

    store expense input values in local state

--- src/components/NewExpense/ExpenseForm.tsx
+++ src/components/NewExpense/ExpenseForm.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './ExpenseForm.css';
 
 const ExpenseForm: React.FC = () => {
+  const [title, setTitle] = useState<string>('');
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.currentTarget.value);
+  };
+
+  const [amount, setAmount] = useState<number>(0.0);
+  const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(Number.parseFloat(e.currentTarget.value));
+  };
+
+  const [date, setDate] = useState<Date>(new Date());
+  const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDate(new Date(e.currentTarget.value));
+  };
+
+  console.log(title, amount, date);
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={handleTitleChange} />
         </div>
       </div>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={handleAmountChange}
+          />
         </div>
       </div>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={handleDateChange}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
