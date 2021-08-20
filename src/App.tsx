974b343ce4f26b39ccf467250e38da3db671a9c4
Robert Wolff <ro.wolff@gmx.net>
2021-08-20 16:22:10 +0200

    Implement adding new expenses

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
+const DUMMY_EXPENSES: Expense[] = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 function App() {
-  const expenses: Expense[] = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState<Expense[]>(DUMMY_EXPENSES);
 
   const handleSaveNewExpense = (newExpense: Expense) => {
-    console.log('From App.js: ');
-    console.log(newExpense);
+    setExpenses((prevExpenses) => [newExpense, ...prevExpenses]);
   };
 
   return (
