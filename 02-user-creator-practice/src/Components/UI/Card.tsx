a9c9e3d051299ad004cf860a9bb17ec8e95616da
Robert Wolff <ro.wolff@gmx.net>
2021-08-26 16:17:43 +0200

    Add styling

---
+++ 02-user-creator-practice/src/Components/UI/Card.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import styled, { FlattenSimpleInterpolation } from 'styled-components';
+
+const CardWrapper = styled.div<{ styles: FlattenSimpleInterpolation }>`
+  background: white;
+  box-shadow: 0 2px 8px rbga(0, 0, 0, 0.26);
+  border-radius: 10px;
+
+  ${({ styles }) => styles}
+`;
+
+interface Props {
+  children: React.ReactNode;
+  styles?: FlattenSimpleInterpolation;
+}
+
+const Card: React.FC<Props> = ({ children, styles }) => {
+  return <CardWrapper styles={styles ? styles : []}>{children}</CardWrapper>;
+};
+
+export default Card;
--- 02-user-creator-practice/src/Components/Users/AddUser.tsx
+++ 02-user-creator-practice/src/Components/Users/AddUser.tsx
@@ -1,4 +1,34 @@
 import React from 'react';
+import { css } from 'styled-components';
+
+import Card from '../UI/Card';
+
+const formStyle = css`
+  margin: 2rem auto;
+  padding: 1rem;
+  width: 90%;
+  max-width: 40rem;
+
+  & label {
+    display: block;
+    font-weight: bold;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    font: inherit;
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    padding: 0.15rem;
+    margin-bottom: 0.5rem;
+  }
+
+  & input:focus {
+    outline: none;
+    border-color: #4f005f;
+  }
+`;
 
 interface Props {}
 
@@ -8,13 +38,15 @@ const AddUser: React.FC<Props> = () => {
   };
 
   return (
-    <form onSubmit={handleAddUser}>
-      <label htmlFor="username">Username</label>
-      <input type="text" id="username" />
-      <label htmlFor="age">Age (years)</label>
-      <input type="number" id="age" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card styles={formStyle}>
+      <form onSubmit={handleAddUser}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" />
+        <label htmlFor="age">Age (years)</label>
+        <input type="number" id="age" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
